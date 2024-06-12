f67f49a39dc1718ae69b1ca13ea27b2995645d06
LucasSchupchek <112569969+LucasSchupchek@users.noreply.github.com>
2024-06-11 22:29:14 -0300

    variaveis de ambiente

--- src/components/dashboard/Chat.vue
+++ src/components/dashboard/Chat.vue
@@ -58,8 +58,7 @@ export default {
     },
     connectSocket() {
       setTimeout(() => {
-        // this.socket = io.connect('http://localhost:3000'); -- para rodar local, descomente essa linha e comente a linha abaixo.
-        this.socket = io.connect('https://apichamados.onrender.com:443');
+        this.socket = io.connect(import.meta.env.VITE_SOCKET_URL);
         this.socket.emit('set_username', this.userName);
 
         this.socket.on('receive_message', message => {
