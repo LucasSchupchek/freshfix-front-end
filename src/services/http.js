4a5b64201dd23f6dd248146b32e3cacc92d04341
LucasSchupchek <112569969+LucasSchupchek@users.noreply.github.com>
2024-06-11 23:09:54 -0300

    vars edit

--- src/services/http.js
+++ src/services/http.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
     // baseURL: 'http://localhost:3000/api/', -- para rodar local, descomente essa linha e comente a linha abaixo.
     // baseURL: 'https://apichamados.onrender.com/api/',
-    baseURL: process.env.VUE_APP_HOST_API,
+    baseURL: import.meta.env.VITE_HOST_API,
     headers: {
         'Content-Type': 'application/json'
     }
