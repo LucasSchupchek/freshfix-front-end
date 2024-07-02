542311f9bbb7d91343124c4078441eeb6568015d
LucasSchupchek <112569969+LucasSchupchek@users.noreply.github.com>
2024-07-02 13:38:15 -0300

    ajustes apresentacao

--- src/components/usuarios/EditUserDialog.vue
+++ src/components/usuarios/EditUserDialog.vue
@@ -5,58 +5,51 @@
         Editar Usuário
         <v-spacer></v-spacer>
       </v-card-title>
+      <v-divider></v-divider>
       <v-card-text>
-        <v-col cols="12" class="d-flex flex-column align-center">
-          <v-avatar
-            size="120"
-            class="mb-2 profile-avatar"
-            @click="triggerFileInput"
-            style="cursor: pointer; position: relative;"
-          >
-            <img :src="profileImage || userCopy.path_avatar || profilePic" class="profile-image"/>
-            <v-file-input
-              ref="fileInput"
-              v-model="profileImageFile"
-              accept="image/*"
-              @change="onImageChange"
-              style="opacity: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%; cursor: pointer;"
-            ></v-file-input>
-          </v-avatar>
-          <small class="profile-description">Adicionar imagem de perfil</small>
-        </v-col>
         <v-form ref="form">
-          <v-text-field v-model="userCopy.nome" label="Nome"></v-text-field>
-          <v-text-field v-model="userCopy.sobrenome" label="Sobrenome"></v-text-field>
-          <v-text-field v-model="userCopy.email" label="Email"></v-text-field>
-          <v-text-field v-model="userCopy.username" label="Username"></v-text-field>
-          <v-select v-model="userCopy.nivel_acesso" :items="accessLevels" label="Nível de Acesso"></v-select>
-          <v-autocomplete
-            v-model="selectedSetor"
-            label="Setor"
-            :items="setores"
-            item-title="descricao"
-            item-value="id"
-            dense
-            v-if="setores.length > 0"
-          ></v-autocomplete>
-          <v-autocomplete
-            v-model="selectedCargo"
-            :items="cargos"
-            item-title="descricao"
-            item-value="id"
-            label="Cargo"
-            dense
-            v-if="cargos.length > 0"
-          ></v-autocomplete>
+          <v-row dense justify="center">
+            <v-col cols="12" class="d-flex flex-column align-center mb-4">
+              <v-avatar size="120" class="mb-2 profile-avatar" @click="triggerFileInput" style="cursor: pointer; position: relative;">
+                <img :src="profileImage || userCopy.path_avatar || profilePic" class="profile-image"/>
+                <v-file-input ref="fileInput" v-model="profileImageFile" accept="image/*" @change="onImageChange" style="opacity: 0; position: absolute; top: 0; left: 0; width: 100%; height: 100%; cursor: pointer;"></v-file-input>
+              </v-avatar>
+              <small class="profile-description">Adicionar imagem de perfil</small>
+            </v-col>
+
+            <v-col cols="12" md="6">
+              <v-text-field v-model="userCopy.nome" label="Nome"></v-text-field>
+            </v-col>
+            <v-col cols="12" md="6">
+              <v-text-field v-model="userCopy.sobrenome" label="Sobrenome"></v-text-field>
+            </v-col>
+            <v-col cols="12" md="6">
+              <v-text-field v-model="userCopy.email" label="Email"></v-text-field>
+            </v-col>
+            <v-col cols="12" md="6">
+              <v-text-field v-model="userCopy.username" label="Username"></v-text-field>
+            </v-col>
+            <v-col cols="12" md="6">
+              <v-select v-model="userCopy.nivel_acesso" :items="accessLevels" label="Nível de Acesso"></v-select>
+            </v-col>
+            <v-col cols="12" md="6">
+              <v-autocomplete v-model="userCopy.setor" label="Setor" :items="setores" item-title="descricao" item-value="descricao" dense></v-autocomplete>
+            </v-col>
+            <v-col cols="12" md="6">
+              <v-autocomplete v-model="userCopy.cargo" :items="cargos" item-title="descricao" item-value="descricao" label="Cargo" dense></v-autocomplete>
+            </v-col>
+          </v-row>
         </v-form>
       </v-card-text>
-      <v-card-actions>
-        <v-btn color="blue darken-1" text @click="fecharDialog">Cancelar</v-btn>
-        <v-btn color="blue darken-1" text @click="saveUser" :loading="loading">Salvar</v-btn>
+      <v-divider></v-divider>
+      <v-card-actions class="justify-end">
+        <v-btn text @click="fecharDialog">Cancelar</v-btn>
+        <v-btn color="primary" text @click="saveUser" :loading="loading">Salvar</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
+
 
 <script>
 import http from '@/services/http.js';
@@ -76,19 +69,21 @@ export default {
     const auth = useAuth();
     const bearer = `Bearer ${auth.token}`;
 
-    const accessLevels = ref(['admin', 'supervisor', 'tecnico', 'default']);
     const userCopy = ref({ ...props.user });
     const setores = ref([]);
     const cargos = ref([]);
     const loading = ref(false); // Adicionando a variável loading
     const profileImageFile = ref(null); // Adicionando o profileImageFile
     const profileImage = ref(null); // Adicionando o profileImage
-    const selectedCargo = ref(null);
-    const selectedSetor = ref(null);
+    const accessLevels = ref(['admin', 'supervisor', 'tecnico', 'default']);
+    const setorMap = ref({});
+    const cargoMap = ref({});
+
 
     onMounted(async () => {
       await loadSetores();
       await loadCargos();
+      setInitialSelections();
     });
 
     const loadSetores = async () => {
@@ -97,6 +92,10 @@ export default {
           headers: { Authorization: bearer }
         });
         setores.value = response.data.result.data.map(item => ({ id: item.id, descricao: item.descricao }));
+        setores.value.forEach(item => {
+          setorMap.value[item.descricao] = item.id;
+        });
+        console.log(setores.value)
       } catch (error) {
         console.error('Erro ao carregar opções de setor:', error);
       }
@@ -108,8 +107,20 @@ export default {
           headers: { Authorization: bearer }
         });
         cargos.value = response.data.result.data.map(item => ({ id: item.id, descricao: item.descricao }));
+        cargos.value.forEach(item => {
+          cargoMap.value[item.descricao] = item.id;
+        });
       } catch (error) {
         console.error('Erro ao carregar opções de cargos:', error);
+      }
+    };
+
+    const setInitialSelections = () => {
+      if (props.user.setor) {
+        userCopy.value.setor = props.user.setor;
+      }
+      if (props.user.cargo) {
+        userCopy.value.cargo = props.user.cargo;
       }
     };
 
@@ -118,23 +129,24 @@ export default {
     };
 
     const saveUser = async () => {
-      
-      console.log()
-
       try {
         const formData = new FormData();
-        console.log(`SETOR ANTES: ${userCopy.value.setor} - SETOR AGORA: ${selectedSetor}`)
-        console.log(`CARGO ANTES: ${userCopy.value.cargo} - CARGO AGORA: ${selectedCargo}`)
-        console.log(userCopy.value)
-        userCopy.value.setor = selectedSetor.value;
-        userCopy.value.cargo = selectedCargo.value;
+        const setorId = setorMap.value[userCopy.value.setor];
+        const cargoId = cargoMap.value[userCopy.value.cargo];
+
+        console.log(userCopy.value.setor)
 
         // Adicione os dados do usuário ao formulário
         Object.keys(userCopy.value).forEach(key => {
-          formData.append(key, userCopy.value[key]);
+          if (key !== 'setor' && key !== 'cargo') {
+            formData.append(key, userCopy.value[key]);
+          }
         });
         // Adicione a nova imagem ao formulário
         formData.append('path_avatar', profileImageFile.value);
+
+        formData.append('setor', setorId);
+        formData.append('cargo', cargoId);
 
         // Enviar solicitação PUT para atualizar o usuário
         const response = await http.put(`/user/${userCopy.value.id}`, formData, {
@@ -172,22 +184,14 @@ export default {
     watch(() => props.user, (newUser) => {
       // Atualize o userCopy ao receber uma nova propriedade de usuário
       userCopy.value = { ...newUser };
+      setInitialSelections();
     });
 
-    return { userCopy, accessLevels, setores, cargos, saveUser, fecharDialog, loading, profilePic, profileImage, onImageChange, profileImageFile, selectedSetor, selectedCargo };
-  },
-  methods: {
-    async updateIsOpen(value) {
-      this.$emit('update:isOpen', value);
-      if (value) {
-        await this.loadSetorOptions();
-        await this.loadSetorOptions();
-        await this.loadCargoOptions();
-      }
-    },
-  },
+    return { userCopy, accessLevels, setores, cargos, saveUser, fecharDialog, loading, profilePic, profileImage, onImageChange, profileImageFile };
+  }
 };
 </script>
+
 
 <style scoped>
 .profile-avatar {
